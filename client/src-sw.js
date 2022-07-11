497dcf5c7c09ed52e30935920d3fab7f1c538d9c
Luis Debourg <Luisdebourgm@gmail.com>
2022-07-11 18:22:51 +1000

    pushing to heroku

--- client/src-sw.js
+++ client/src-sw.js
@@ -4,6 +4,8 @@ const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { StaleWhileRevalidate } = require('workbox-strategies');
+
 
 precacheAndRoute(self.__WB_MANIFEST);
 
@@ -26,5 +28,16 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    // Name of the cache storage.
+    cacheName: 'asset-cache',
+    plugins: [
+      // cache maximum-age of 30 days
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,25 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
-initdb();
+  export const putDb = async (content) => {
+    const jateDb = await openDB('jate', 1);
+    console.log(jateDb);
+    const jx = jateDb.transaction('jate', 'readwrite');
+    const store = jx.objectStore('jate');
+    const request = store.put({ jate: content });
+    const result = await request;
+    console.log('data saved to the database', result);
+  }
+  
+  export const getDb = async () => {
+    const jateDb = await openDB('jate', 1);
+    console.log(jateDb);
+    const jx = jateDb.transaction('jate', 'readonly');
+    const store = jx.objectStore('jate');
+    const request = store.getAll();
+    const result = await request;
+    console.log('result.value', result);
+    return result;
+  };
+  
+  initdb();
